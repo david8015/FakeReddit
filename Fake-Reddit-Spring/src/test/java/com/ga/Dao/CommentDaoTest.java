22179bf79fb526ac4b6b1bc4708761e138dd862c
David Hernandez <david.hernandez8015@gmail.com>
2019-10-26 16:59:25 -0400

    user dao test

--- Fake-Reddit-Spring/src/test/java/com/ga/Dao/CommentDaoTest.java
+++ Fake-Reddit-Spring/src/test/java/com/ga/Dao/CommentDaoTest.java
@@ -2,9 +2,12 @@ package com.ga.Dao;
 
 import com.ga.dao.CommentDaoImpl;
 import com.ga.entity.Comment;
+import com.ga.entity.Post;
+import com.ga.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -13,6 +16,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.junit.Before;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 public class CommentDaoTest {
@@ -29,13 +33,30 @@ public class CommentDaoTest {
     Transaction transaction;
 
     @InjectMocks
-    Comment comment;
+    private Comment comment;
+
+    @Mock
+    User user;
+
 
     @InjectMocks
     private CommentDaoImpl commentDao;
 
+    @InjectMocks
+    Post post;
+
+    @Mock
+    Query<User> query;
+
+    @Mock
+    Query<Comment> commentQuery;
+
     @Before
     public void init() {
+        post.setId(1L);
+        post.setTitle("test");
+        post.setDescription("description");
+
         comment.setId(1L);
         comment.setDescription("description");
 
@@ -48,7 +69,10 @@ public class CommentDaoTest {
         String username = "username";
         Long postid = 1l;
 
+        when(session.createQuery(anyString())).thenReturn(query);
+        when(query.getSingleResult()).thenReturn(user);
+        when(session.get(anyLong()).thenReturn(post);
 
-    }
+        }
 
     }
